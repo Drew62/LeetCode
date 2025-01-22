7f2c3691306bd5100e5715998e207363382752f8
Drew62 <drew.pickett62@gmail.com>
2025-01-21 20:58:44 -0500

    + Reversed Linked List

---
+++ NeetCode/Easy/Reverse_Linked_List.py
@@ -0,0 +1,17 @@
+# Given the beginning of a singly linked list head, reverse the list, and return the new beginning of the list.
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev, curr = None, head
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
